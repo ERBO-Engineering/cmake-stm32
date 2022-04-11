1e583d1fc6f3fd4f2b083607a2534149c5113d57
Bob Thomas <bobthomas295@gmail.com>
2022-04-11 22:58:24 +0200

    add empty main

---
+++ src/main.c
@@ -0,0 +1,14 @@
+int main(void)
+{
+    // ...
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1)
+    {
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
+}
