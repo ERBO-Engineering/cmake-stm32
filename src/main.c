9d8cabe055ef12481e9e99e4813fe792e3991167
Bob Thomas <bobthomas295@gmail.com>
2022-05-26 12:24:21 +0200

    Add really simple blink example

--- src/main.c
+++ src/main.c
@@ -1,86 +1,36 @@
-#include "stm32f411xe.h"
+#include "stm32f4xx.h"
 
-void delay(uint32_t time);
-void setupSystem(void);
-void setupGPIO(void);
-
-int main(void)
-{
-  setupSystem();
-  setupGPIO();
-
-  while (1)
-  {
-    GPIOD->BSRR |= GPIO_BSRR_BS_12;
-    delay(1000000);
-    GPIOD->BSRR |= GPIO_BSRR_BR_12;
-    delay(1000000);
-  }
-}
-
-void setupSystem(void)
-{
-
-#define PLL_M 4
-#define PLL_N 72
-#define PLL_P 1 // PLLP = 2
-#define PLL_Q 4
-
-  // setup HSE
-  RCC->CR |= RCC_CR_HSEON;
-
-  // wait until ready
-  while (!(RCC->CR & RCC_CR_HSERDY))
-    ;
-
-  // set the power enable clock and voltage regulator
-  RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-  PWR->CR |= PWR_CR_VOS;
-
-  // setup flash and latency
-  FLASH->CR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_3WS;
-
-  // setup prescalars
-  RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
-
-  // PLCK1
-  RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
-  RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
-
-  // setup pll
-  RCC->PLLCFGR = (PLL_M << 0) | (PLL_N << 6) | (PLL_P << 16) | RCC_PLLCFGR_PLLSRC_HSE;
-
-  // ENABLE PLL
-  RCC->CR |= RCC_CR_PLLON;
-
-  // Wait for PLL to be ready
-  while (!(RCC->CR & RCC_CR_PLLRDY))
-    ;
-
-  // Set clock source and wait for ready
-  RCC->CFGR |= RCC_CFGR_SW_PLL;
-  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL)
-  {
-    __NOP();
-  }
-}
-
-void setupGPIO(void)
-{
-  // ENABLE bank D
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-
-  // Set output for 15 pd15 blue led
-  GPIOD->MODER |= GPIO_MODER_MODE15_0;
-  GPIOD->MODER |= GPIO_MODER_MODE12_0;
-  GPIOD->OTYPER = 0;
-  GPIOD->OSPEEDR = 0;
-}
-
-void delay(uint32_t time)
-{
-  while (time--)
-  {
-    __NOP();
-  }
+// https://learnembeddedsystems.co.uk/stm32f4-discovery-beginners-tutorial-blink-leds
+int main(){
+	
+	// Loop Variables
+	uint32_t i;
+	
+	// Configue LEDs
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // Enable the clock of port D of the GPIO
+	
+	GPIOD->MODER |= GPIO_MODER_MODER12_0; // Green LED, set pin 12 as output
+	GPIOD->MODER |= GPIO_MODER_MODER13_0; // Orange LED, set pin 13 as output
+	GPIOD->MODER |= GPIO_MODER_MODER14_0; // Red LED, set pin 14 as output
+	GPIOD->MODER |= GPIO_MODER_MODER15_0; // Blue LED, set pin 15 as output
+	
+	while(1){
+		// Turn on LEDs
+		GPIOD->BSRR = 1<<12; // Set the BSRR bit 12 to 1 to turn respective LED on
+		GPIOD->BSRR = 1<<13; // Set the BSRR bit 13 to 1 to turn respective LED on
+		GPIOD->BSRR = 1<<14; // Set the BSRR bit 14 to 1 to turn respective LED on
+		GPIOD->BSRR = 1<<15; // Set the BSRR bit 15 to 1 to turn respective LED on
+		
+		// Delay
+		for(i = 0; i < 200000; i++){}; // Loop repeats 2,000,000 implementing a delay
+			
+		// Turn off LEDs
+		GPIOD->BSRR = 1<<(12+16); // Set the BSRR bit 12 + 16 to 1 to turn respective LED off
+		GPIOD->BSRR = 1<<(13+16); // Set the BSRR bit 13 + 16 to 1 to turn respective LED off
+		GPIOD->BSRR = 1<<(14+16); // Set the BSRR bit 14 + 16 to 1 to turn respective LED off
+		GPIOD->BSRR = 1<<(15+16); // Set the BSRR bit 15 + 16 to 1 to turn respective LED off
+				
+		// Delay
+		for(i = 0; i < 200000; i++){}; // Loop repeats 2,000,000 implementing a delay
+	}
 }
