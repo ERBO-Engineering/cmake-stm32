7bb8dd43bdc4a28a89afc2a1c40f349c1b296d18
erwin <erwinb1993@gmail.com>
2023-08-04 14:59:38 +0200

    working HSE

--- BSP/Core/Src/stm32f4xx_it.c
+++ BSP/Core/Src/stm32f4xx_it.c
@@ -199,6 +199,19 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
+  * @brief This function handles RCC global interrupt.
+  */
+void RCC_IRQHandler(void)
+{
+  /* USER CODE BEGIN RCC_IRQn 0 */
+
+  /* USER CODE END RCC_IRQn 0 */
+  /* USER CODE BEGIN RCC_IRQn 1 */
+
+  /* USER CODE END RCC_IRQn 1 */
+}
+
+/**
   * @brief This function handles USB On The Go FS global interrupt.
   */
 void OTG_FS_IRQHandler(void)
